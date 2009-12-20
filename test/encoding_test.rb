3a5524896bee96c70b38596e5c34699471def278
Manfred Stienstra <manfred@fngtps.com>
2009-12-20 18:55:07 +0100

    Add more assertions with different encodings.
    Adds more source and target encoding examples to cover more scenarios.

--- test/encoding_test.rb
+++ test/encoding_test.rb
@@ -8,12 +8,23 @@ $:.unshift(File.expand_path('../../lib', __FILE__))
 require 'encoding'
 
 module EncodingTestHelpers
+  EXAMPLES = {
+    'UTF-8'      => '℻',
+    'ISO-8859-1' => 'Café',
+    'Shift_JIS'  => 'こにちわ'
+  }
+  
+  def examples
+    EXAMPLES.keys
+  end
+  
   def example(name, options={})
-    example = ''
-    File.open(File.expand_path("../examples/#{name}.txt", __FILE__), 'r:binary') do |file|
-      example << file.read
+    filename, contents = name.gsub(/-/, '_').downcase, ''
+    File.open(File.expand_path("../examples/#{filename}.txt", __FILE__), 'r:binary') do |file|
+      contents << file.read
     end
-    example.force_encoding(options[:read_as] || Encoding::ASCII_8BIT)
+    contents.force_encoding(options[:read_as] || Encoding::ASCII_8BIT)
+    return contents, EXAMPLES[name]
   end
 end
 
@@ -21,26 +32,36 @@ class EncodingTest < ActiveSupport::TestCase
   include EncodingTestHelpers
   
   test "source reports wrong encoding but is actually preferred encoding" do
-    example = example('utf_8', :read_as => 'US-ASCII')
-    example.ensure_encoding('UTF-8')
-    assert_equal '℻', example
+    (Encoding.list - [Encoding::UTF_8]).each do |source_encoding|
+      examples.each do |e|
+        source, expected = example(e, :read_as => source_encoding)
+        source.ensure_encoding('UTF-8')
+        assert_equal(expected, source)
+      end
+    end
   end
   
-  test "source has the wrong encoding but reports the correct encoding" do
-    example = example('iso_8859_1', :read_as => 'ISO-8859-1')
-    example.ensure_encoding('UTF-8')
-    assert_equal 'Café', example
+  test "source has the wrong encoding but properly reports its own encoding" do
+    (examples - ['UTF-8']).each do |e|
+      source, expected = example(e, :read_as => e)
+      source.ensure_encoding('UTF-8')
+      assert_equal(expected, source)
+    end
   end
   
   test "source has the wrong encoding and unjustly reports the target encoding" do
-    example = example('iso_8859_1', :read_as => 'UTF-8')
-    example.ensure_encoding('UTF-8')
-    assert_equal 'Café', example
+    (examples - ['UTF-8']).each do |e|
+      source, expected = example(e, :read_as => 'UTF-8')
+      source.ensure_encoding('UTF-8')
+      assert_equal(expected, source)
+    end
   end
   
   test "source has the wrong encoding and reports something different from its actual encoding" do
-    example = example('iso_8859_1', :read_as => 'Shift_JIS')
-    example.ensure_encoding('UTF-8')
-    assert_equal 'Café', example
+    (examples - ['UTF-8']).each do |e|
+      source, expected = example(e, :read_as => Encoding::KOI8_R)
+      source.ensure_encoding('UTF-8')
+      assert_equal(source, expected)
+    end
   end
 end
